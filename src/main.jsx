1fd822553a598037ad45613a2ef68958f783aa62
Witzevds <witzvand@student.arteveldehs.be>
2024-12-06 16:04:09 +0100

    changed paths

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./pages/App/App.jsx";
 import Scanner from "./pages/Scanner/Scanner.jsx";
-import Webshop from "./pages/webshop/Webshop.jsx";
+import Webshop from "./pages/Webshop/Webshop.jsx";
 import Footer from "./sections/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
 
