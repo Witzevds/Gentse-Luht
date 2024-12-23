42f697f9291026f9f5480b07915c000ef3a1a74e
Witzevds <witzvand@student.arteveldehs.be>
2024-12-23 16:02:11 +0100

    new

--- src/sections/Hero-main/Hero-main.jsx
+++ src/sections/Hero-main/Hero-main.jsx
@@ -13,17 +13,28 @@ const Hero = () => {
       </div>
       <div className="hero__display">
         <a-scene embedded>
-          <a-light type="directional" intensity="1" position="2 0 0"></a-light>
+          <a-light
+            type="directional"
+            intensity="1"
+            position="0 20 10"
+          ></a-light>
           <a-light type="ambient" intensity="0.5"></a-light>
-          <a-camera position="-2 2 0"></a-camera>
 
-          {/* 3D-model met textuur */}
+          {/* Camera with orbit-controls */}
+          <a-camera
+            position="0 3 10"
+            fov="50"
+            orbit-controls="target: 0 2 -2; enableDamping: true; maxDistance: 15; minDistance: 5;"
+          ></a-camera>
+
+          {/* 3D-model with infinite rolling animation */}
           <a-entity
-            gltf-model="url(/Variables/Assets/bottle-3D.glb)"
-            position="0 2 -3"
-            scale="0.3 0.3 0.3"
-            rotation="0 15 90"
+            gltf-model="url(/Variables/Assets/new-3D-bottle.glb)"
+            position="0 3 0"
+            scale="1 1 1"
+            rotation="90 0 0"
             material="src: url(/Variables/Assets/bottle-hero-display.png);"
+            animation="property: rotation; to: 90 360 0; loop: true; dur: 5000;"
           ></a-entity>
         </a-scene>
       </div>
